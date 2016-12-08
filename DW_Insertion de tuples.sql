a1b16740a570157ddc7ec2fc7405fb70bffd27e1
mohyas13 <moh_yas@hotmail.fr>
2016-12-08 18:53:29 +0100

    Add files via upload

--- DW_Insertion de tuples.sql
+++ DW_Insertion de tuples.sql
@@ -21,7 +21,7 @@ INSERT INTO D_client VALUES(
 
 
 INSERT INTO D_remise VALUES(
-	2, 'remise de 50%', 'r50', 'remise de fin d"année', 0.5, '01/12/2016', '31/12/2016', 1, 'Monde');
+	2, 'remise de 50%', 'r50', 'remise de fin d\\''année', 0.5, '01/12/2016', '31/12/2016', 1, 'Monde');
 
 INSERT INTO D_remise VALUES(
 	1, 'remise de 20%', 'r20', 'remise de -25 ans en France', 0.2, NULL, NULL, NULL, 'France');
@@ -32,24 +32,24 @@ INSERT INTO D_remise VALUES(
 
 
 INSERT INTO D_produit VALUES(
-	1, 'Pain complet', 'Pain fabriqué à partir de la farine', 'Pain', 'Pain', 'Planète pain', 0.12, 400, 'Sachet', 1.20,);
+	1, 'Pain complet', 'Pain fabriqué à partir de la farine', 'Pain', 'Complet', 'Planète pain', 0.12, 400, 'Sachet', 1.20,'France');
 
 INSERT INTO D_produit VALUES(
-	2, 'Chorizo', 'Saucisson origine espagnole fabriqué à partir de viande de porc', 'Saucisse', 'Chorizo de Léon', 'Portalconsa', 0.38, 100, 'Film', 0.95);
+	2, 'Chorizo', 'Saucisson origine espagnole fabriqué à partir de viande de porc', 'Saucisse', 'Chorizo de Léon', 'Portalconsa', 0.38, 100, 'Film', 0.95, 'Espagne');
 
 INSERT INTO D_produit VALUES(
-	3, 'Cros Parantoux', 'Vin rouge - Vosne-Romanée Premier Cru Cros Parantoux', 'Vin', 'Rouge', 'Henri Jayer Richebourg', 0, 0.75, 'Boîtes', 1380);
+	3, 'Cros Parantoux', 'Vin rouge - Vosne-Romanée Premier Cru Cros Parantoux', 'Vin', 'Rouge', 'Henri Jayer Richebourg', 0, 0.75, 'Boîtes', 1380, 'France');
 
 
 
 INSERT INTO D_allergie VALUES(
-	1, 'Intolérant au gluten', 'souvent dès la naissance', 2, 'Céréales contenant du gluten', 'Cancer des intestins');
+	1, 'Intolérant au gluten', 'souvent dès la naissance', 'Gluten');
 
 INSERT INTO D_allergie VALUES(
-	2, 'Intolérant au lactose', 'se développe assez souvent chez les jeunes', 0, 'Produits à base de lactose', 'Maladie coeliaque');
+	2, 'Intolérant au lactose', 'se développe assez souvent chez les jeunes', 'Lactose');
 
 INSERT INTO D_allergie VALUES(
-	3, 'Intolérant au sésame', 'souvent dès la naissance', 1, 'Produits à base de sésame', 'Eczéma');
+	3, 'Intolérant au sésame', 'souvent dès la naissance', 'Sésame');
 
 
 
@@ -62,3 +62,13 @@ INSERT INTO D_clientAllergieBridge VALUES(
 INSERT INTO D_clientAllergieBridge VALUES(
 	3, 3, 3, 0.2);
 
+
+
+INSERT INTO D_plateforme VALUES(
+	1, 'Application mobile', 120, 'TerroirApp', 'Application pour smartphone', '24/04/2012', 20000, '08/12/2016');
+
+INSERT INTO D_plateforme VALUES(
+	2, 'Site web', 300, 'www.produitterroir.fr', 'Site web de l\\''entreprise', '15/06/2007', 75000, '08/12/2016');
+
+INSERT INTO D_plateforme VALUES(
+	1, 'Téléphone', 80, NULL, 'Ligne téléponique pour des commandes', '15/06/2007', NULL, NULL);
