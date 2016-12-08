55aff803d4c1f8f3054dfd17d87470c8d9db2d3b
Hani <h.guenoune@gmail.com>
2016-12-08 13:52:29 +0100

    Adding DM1 tables creation script

--- DM1_Tables_creation_Script.sql
+++ DM1_Tables_creation_Script.sql
@@ -32,18 +32,16 @@ CREATE TABLE F_commande
 	produit_id number,
 	remise_id number,
 	client_id number,
-	--local_id number,
 	date_id number,
 	plateau_code number
 	prix_vente number,
 	cout_reel number,
 	benefice_net number,
 	PRIMARY KEY (produit_id,plateau_code);
-	CONSTRAINT fkey_produit_id FOREIGN KEY (produit_id) REFERENCES D_produit(produit_id),
+	CONSTRAINT fkey_produit_id FOREIGN KEY (produit_id) REFERENCES D_prodForComm(vc_produit_id),
 	CONSTRAINT fkey_remise_id FOREIGN KEY (remise_id) REFERENCES D_remise(remise_id),
 	CONSTRAINT fkey_client_id FOREIGN KEY (client_id) REFERENCES D_client(client_id),
-	--CONSTRAINT fkey_local_id FOREIGN KEY (local_id) REFERENCES D_local(local_id),
-	CONSTRAINT fkey_date_id FOREIGN KEY (date_id) REFERENCES D_date(date_id)
+	CONSTRAINT fkey_date_id FOREIGN KEY (date_id) REFERENCES D_dateForComm(vc_date_id)
 );
 
 /*
@@ -102,23 +100,7 @@ CREATE TABLE D_remise
 	portee_geographique varchar(50),
 	PRIMARY KEY (remise_id),
 );
-/*
-CREATE TABLE D_local
-(
-	local_id number,
-	nom varchar(15),
-	region varchar(15),
-	adresse varchar(50),
-	code_postal varchar(5),
-	capacite_stockage number,
-	nbr_employes number,
-	nom_responsable varchar(15),
-	superficie number,
-	nbr_remises number,
-	classement number,
-	PRIMARY KEY (local_id),
-);
-*/
+
 CREATE TABLE D_produit
 (
 	produit_id number,
@@ -226,37 +208,4 @@ fiscal_year_begin_date_key integer NULL,
 fiscal_year_end_date date NULL,
 fiscal_year_end_date_key integer NULL
 );
-
-/*
-
-Création des vues pour les dimensions partagées.
-<dimension>s partagées : D_produit, D_date.
-
-Une vue pour chaque table de fait référençant la dimension ; 
-<dimension>ForComm : attributs --> vc_<nom_attribut>.
-<dimension>ForStocks : attributs --> vs_<nom_attribut>.
-
-*/
-
---D_produit
-
-
-CREATE VIEW D_prodForComm (vc_produit_id,vc_nom,vc_description,vc_category,vc_type,vc_fournisseur,vc_matiere_grasse,vc_poids_unitaire,vc_emballage,vc_prix_unitaire) AS
-SELECT produit_id,nom,description,category ,type,fournisseur,matiere_grasse,poids_unitaire,emballage,prix_unitaire 
-FROM D_produit
-
-
-CREATE VIEW D_prodForStocks (vs_produit_id,vs_nom,vs_description,vs_category,vs_type,vs_fournisseur,vs_matiere_grasse,vs_poids_unitaire,vs_emballage,vs_prix_unitaire) AS
-SELECT produit_id,nom,description,category,type,fournisseur,matiere_grasse,poids_unitaire,emballage,prix_unitaire 
-FROM D_produit
-
---D_date
-
-CREATE VIEW D_dateForComm (vc_date_id,vc_date_type,vc_full_date,vc_day_num_of_week,vc_day_num_of_month,vc_day_num_of_quarter,vc_day_num_of_year,vc_day_num_absolute,vc_week_num_of_month,vc_week_num_of_quarter,vc_week_num_of_year,vc_week_num_overall,vc_month_num_of_year,vc_month_num_overall) AS
-SELECT date_id,date_type,full_date,day_num_of_week ,day_num_of_month ,day_num_of_quarter ,day_num_of_year ,day_num_absolute,week_num_of_month ,week_num_of_quarter ,week_num_of_year ,week_num_overall ,month_num_of_year ,month_num_overall
-FROM D_date
-
-CREATE VIEW D_dateForStocks (vs_date_id,vs_date_type,vs_full_date,vs_day_num_of_week,vs_day_num_of_month,vs_day_num_of_quarter,vs_day_num_of_year,vs_day_num_absolute,vs_week_num_of_month,vs_week_num_of_quarter,vs_week_num_of_year,vs_week_num_overall,vs_month_num_of_year,vs_month_num_overall) AS
-SELECT date_id,date_type,full_date,day_num_of_week ,day_num_of_month ,day_num_of_quarter ,day_num_of_year ,day_num_absolute,week_num_of_month ,week_num_of_quarter ,week_num_of_year ,week_num_overall ,month_num_of_year ,month_num_overall
-FROM D_date
 
